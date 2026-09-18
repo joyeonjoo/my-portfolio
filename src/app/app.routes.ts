9d3eaf48f69b49bba362c836fe02ecbd1d2ef2bc
joyeonjoo <65939709+joyeonjoo@users.noreply.github.com>
2026-09-18 11:46:16 +0900

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
+import { DesignSystemComponent } from './pages/design-system/design-system.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'projects/:id', component: ProjectDetailComponent },
+  { path: 'design-system', component: DesignSystemComponent },
   { path: '**', redirectTo: '' },
 ];
